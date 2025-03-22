9c03f9c2e689c3093765e1f0359a6042a7d0d851
Abbhishekbehera <abbishekbehara.ab@gmail.com>
2025-03-22 16:14:33 +0530

    First commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/task_manager');
+    await mongoose.connect('mongodb+srv://behera:12345@cluster0.pe2oc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 }
 
 app.listen(4000, () => {
